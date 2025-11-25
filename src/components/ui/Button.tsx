ffee8b84d2dd02a8924857feae7dad90e94a161f
Benny Gebauer <benny.gebauer@outlook.de>
2025-11-25 09:24:49 +0100

    init button component

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,22 @@
+import "@sass/components/ui/button.sass";
+
+export interface ButtonProps {
+	children: React.ReactNode;
+	variant: "solid" | "outline" | "ghost";
+	color: "primary" | "neutral";
+	link?: string;
+	size?: "sm" | "md" | "lg";
+	onClick?: () => void;
+}
+export const Button = ({ children, variant, color, onClick }: ButtonProps) => {
+	return (
+		<button
+			className="button inline-flex gap-100 items-center"
+			data-variant={variant}
+			data-color={color}
+			onClick={onClick}
+		>
+			{children}
+		</button>
+	);
+};
